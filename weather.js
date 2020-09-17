194a01c3fc6ece570a742721f09c81bc78c0f89e
inyong <jeong4726@gmail.com>
2020-09-17 17:41:02 +0900

    Update weather.js

---
+++ weather.js
@@ -0,0 +1,25 @@
+const COORDS = "coords";
+
+function handleGeoSucces(position) {
+  console.log(position);
+}
+function handleGeoError() {
+  console.log("can't find geo location");
+}
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+    //getweather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
